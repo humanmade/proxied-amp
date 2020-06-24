e8a9cb9e473d1be92c200ebaa758059bed942fa2
Abhishek Kaushik <abhishek@humanmade.com>
2020-06-24 13:27:12 +0530

    Intial Push

---
+++ inc/namespace.php
@@ -0,0 +1,12 @@
+<?php
+
+declare( strict_types=1 );
+
+namespace HM\Proxied_AMP;
+
+/**
+ * Plugin BootStrapper.
+ *
+ * @return void
+ */
+function bootstrap () : void {}
---
+++ plugin.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Plugin Name: Proxied AMP
+ * Description:
+ * Version: 0.0.1
+ * Plugin URI: https://humanmade.com
+ * Author: Human Made
+ * Author URI: https://humanmade.com
+ */
+
+namespace HM\Proxied_AMP;
+
+// Libraries.
+require_once __DIR__ . '/inc/namespace.php';
+
+bootstrap();
